8214452865cafb9f1118bc36032670e6ee1153c5
MartinPankraz <martin.pankraz@convista.com>
2018-02-02 16:56:07 +0100

    sidenavigation update (horizontal overflow)

---
+++ com.convista.sidenavigation/res/additional_properties_sheet/additional_properties_sheet.js
@@ -0,0 +1,29 @@
+sap.designstudio.sdk.PropertyPage.subclass("com.convista.sidenavigation.APS",  function() {
+
+	var that = this;
+	
+	this.init = function() {
+//		eclipse_logJavaScriptMessage("init","error");
+	};
+	
+	this.afterUpdate = function() {
+//		eclipse_logJavaScriptMessage("update0","error");
+//		$("#aps_placeholder").change(function() {
+//			that.firePropertiesChanged(["DPlaceholder"]);
+//			return false;
+//		});
+//		eclipse_logJavaScriptMessage("update1","error");
+	}
+
+//	this.DPlaceholder = function(value) {
+//		if (value === undefined) {
+//			return $("#aps_placeholder").val();
+//		}
+//		else {
+//			$("#aps_placeholder").val(value);
+//			return this;
+//		}
+//	};
+
+	
+});
